bf7ad825682dbd388c66f7b2692a03a6b4064f2e
coque <coquelet.samuel@gmail.com>
2022-02-27 21:05:57 +0100

    fini

--- index.php
+++ index.php
@@ -4,6 +4,19 @@
  * 1. Commencez par importer le script SQL disponible dans le dossier SQL.
  * 2. Connectez vous à la base de données blog.
  */
+$server = 'localhost';
+$db = 'blog';
+$user = 'root';
+$pwd = '';
+
+try {
+    $bdd = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pwd);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $exception) {
+    echo $exception->getMessage();
+}
 
 /**
  * 3. Sans utiliser les alias, effectuez une jointure de type INNER JOIN de manière à récupérer :
@@ -16,14 +29,34 @@
  * A l'aide d'une boucle, affichez chaque ligne du tableau de résultat.
  */
 
-// TODO Votre code ici.
+$request = $bdd->prepare("
+    SELECT article.id, article.title, article.content, categorie.name
+    FROM article
+    INNER JOIN categorie ON article.category_fk = categorie.id
+");
+
+$request->execute();
+
+echo "<pre>";
+print_r($request->fetchAll());
+echo "</pre><br><br>";
 
 
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
  */
 
-// TODO Votre code ici.
+$request = $bdd->prepare("
+    SELECT ar.id, ar.title, ar.content, ca.name
+    FROM article as ar
+    INNER JOIN categorie as ca ON ar.category_fk = ca.id
+");
+
+$request->execute();
+
+echo "<pre>";
+print_r($request->fetchAll());
+echo "</pre><br><br>";
 
 
 /**
@@ -32,4 +65,25 @@
  *    Avec un LEFT JOIN, affichez tous les commentaires et liez le nom et le prénom de l'utilisateur ayant écris le comentaire.
  */
 
-// TODO Votre code ici.
+$stmt = $bdd->prepare("INSERT INTO utilisateur VALUES (null, 'Samuel', 'Coquelet', 'coquelet.samuel@mail.fr', 'SamSam')");
+
+$stmt->execute();
+
+$stmt = $bdd->prepare("INSERT INTO commentaire 
+                            VALUES (null, 'Bonjour, voici un commentaire', 1, 1),
+                                   (null , 'Mon petit commentaire ahah', 15, 2)
+                            ");
+
+$stmt->execute();
+
+$request = $bdd->prepare("
+    SELECT co.content, ut.firstName, ut.lastName
+    FROM commentaire as co
+    LEFT JOIN utilisateur as ut ON ut.id = co.user_fk
+");
+
+$request->execute();
+
+echo "<pre>";
+print_r($request->fetchAll());
+echo "</pre><br><br>";
